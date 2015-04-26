846412c3066b9ff81139c1a13d8af8d07dbffd58
mdsanders <mattds@uga.edu>
2015-04-26 11:28:25 -0400

    Work on file picker

--- termProject/profile.php
+++ termProject/profile.php
@@ -72,16 +72,41 @@ include "session.php";
         if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
           var doc = data[google.picker.Response.DOCUMENTS][0];
           url = doc[google.picker.Document.URL];
+          sendRequest(url);
         }
-        var xmlhttp;
-        if(window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        }
-        xmlhttp.open("POST", "profile.php", true);
-		xmlhttp.send(url);            
       }
-    </script>	
 
+      function sendRequest(url) {
+   		var xmlhttp = new XMLHttpRequest();
+   		xmlhttp.open("GET", "savedocument.php?url=" + url, true);
+   		xmlhttp.send();
+   		xmlhttp.onreadystatechange=function() {
+   			window.location = "http://www.opero.us/profile.php?" + (Math.random() * 10);
+   		}	
+      }
+
+      function deleteResume() {
+		var table = document.getElementById('resumes');
+		var checked = new Array();
+		var len = table.rows.length;
+		for(var i = 1; i < len; i++) {
+			if(document.getElementById("resumecheck" + i).checked)
+				checked[i] =  document.getElementById("resume" + i);	
+		}
+
+		var xmlhttp = new XMLHttpRequest();
+		var url = "?";
+		var count = 0;
+		for(var i = 1; i < len; i++) {
+			if(checked[i] !== undefined) {
+				count++;
+				url += "link" + count + "=" + checked[i] + "&";
+			}	
+		}	
+		xmlhttp.open("GET", "deletedocument.php" + url, true);
+		xmlhttp.sent();
+      }            
+    </script>	
 </head>
 <body>
 	<div id="profileHeader">
@@ -89,10 +114,12 @@ include "session.php";
 	
 	
 	<div id = "linkbox">
+	<a href="index.php">Home</a>
 	<a href="setUserPrefs.php">Preferences</a>
 	
 	<!--set preferences should include number of fave jobs to display
 	Pref should be saved as $_SESSION['faveJobCount']-->
+	
 	<!-- The Google API Loader script. -->
 	<script type="text/javascript" src="https://apis.google.com/js/api.js"></script>
 	
@@ -100,8 +127,6 @@ include "session.php";
 	<a href="changePassword.php">Change Password</a><br>
 	<a href="logout.php">Log Out</a><br>
 	
-	<button onclick="onApiLoad()">Upload Resume</button><br>
-
 	</div><!--end linkbox-->
 	
 	</div><!--end profile header-->
@@ -109,13 +134,51 @@ include "session.php";
 	<!--php script to output favorite jobs to an html table-->
 	<!--settings for output can be adjusted in user preferences php page-->
 	<div ="favejobsTable">
-	<?php include "faveJobs.php";?>
+	<!--include "faveJobs.php";-->
 	</div>
 	
 	<?php 
-	$url = $_POST["url"];
-	echo url;	
+	$user_name = "a";
+	$password = "Csci4300";
+	$hostname = "localhost";
+	$currentuser = $_SESSION['login_user'];
+	
+	try {
+		$db = new PDO("mysql:host=$hostname;dbname=Documents", "a", "Csci4300");
+		$sql = "SELECT * FROM resumes WHERE username = '$currentuser'";
+		
+		$stmt = $db->query($sql);
+		
+		$count = 1;
+		if($stmt != NULL) {
+			echo "<br><br><br><table id='resumes'>";
+			echo "<tr><th>Your Resumes</th></tr>";
+			while($row = $stmt->fetchObject()) {
+	      		echo "<tr>";
+				$resumeURL = $row->url;
+				echo "<td><input type='checkbox' id='resumecheck" . $count . "'>";
+				echo "<td><a href='" . $resumeURL . "' id='resume" . $count . "'>Resume" . $count . "</td>";
+				echo "</tr>";
+				$count++;
+			}
+			echo "</table>";
+		}
+		/* foreach($db->query(sql) as $row) {
+			echo $row['username'].' '.$row['url']; //etc...
+		} */
+		
+		$db = null;
+	}
+	catch(PDOException $e)
+	{
+		echo $e->getMessage();
+	}
 	?>
+	
+	<button onclick="onApiLoad()">Upload New Resume</button>
+	<button onclick="deleteResume()">Delete Selected Resume</button><br>
+	
+	<div id="result"></div>
 	
 	<!--terminado-->
 	
--- termProject/savedocument.php
+++ termProject/savedocument.php
@@ -1,1 +1,30 @@
-
+<!DOCTYPE html>
+<html>
+<?php 
+include "session.php";
+?>	
+	<head></head>
+	
+	<body>
+	<?php
+	 $username = $_SESSION['login_user'];
+	 $url = $_GET['url'];
+	 
+	 $user_name = "a";
+	 $password = "Csci4300";
+	 $hostname = "localhost";
+	 
+	 try {
+	 	$db = new PDO("mysql:host=$hostname;dbname=Documents", "a", "Csci4300");
+		$sql = "INSERT INTO resumes(username, url) VALUES ('$username', '$url')";
+	 	$count = $db->exec($sql);
+	 	
+		$db = null;
+	 }
+	 catch(PDOException $e)
+	 {
+	 	echo $e->getMessage();
+	 }
+	?>
+	</body></html>
+	
